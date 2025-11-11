12754744c4cc6c9ff1b6f04d77254569ec290004
Menghetz <manuelmenghetti0@gmail.com>
2025-11-11 18:52:09 +0100

    Add Puntatori.c

---
+++ puntatori.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NUMBERS 10000
+
+typedef struct {
+	int* item;      // Puntatore all'array dinamico
+	int size;       // Numero di elementi attualmente presenti
+	int capacity;   // Capacità  massima allocata
+} DynamicArray;
+
+void append(DynamicArray* array, int valore) {
+	if(array->capacity == array->size) {
+		if (array->capacity == 0) {
+			array->capacity++;
+		}
+		array->capacity *= 2;
+		array->item = realloc(array->item, array->capacity * sizeof(int));
+		if (array->item == NULL) {
+			fprintf(stderr, "Error while reallocating memory!");
+			exit(EXIT_FAILURE);
+		}
+		
+	}
+	array->item[array->size] = valore;
+	array->size++;
+}
+
+int main()
+{
+	DynamicArray* array = malloc(sizeof(DynamicArray));
+	array->capacity = 0;
+	array->size = 0;
+	array->item = NULL;
+
+	for (int i = 1; i <= NUMBERS; i++) {
+		append(array, i);
+	}
+
+	for (int i = 0; i < NUMBERS; i++) {
+		printf("%d ", array->item[i]);
+	}
+	
+	free(array->item); 
+	free(array);
+    
+	return 0;
+}
